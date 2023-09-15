8bd372e0c2c643052ac94c179684fd40de64c897
roc-11 <>
2023-09-15 14:10:50 +0000

    Display addition to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,10 +22,13 @@ document.addEventListener("DOMContentLoaded", function () {
                 //set the gameType to the value of that attribute
                 //this will tell us which gameType to run
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                //alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    //default is for addition game to load
+    runGame("addition");
 });
 
 //Math.random() * 25; random number between 1 & 25
@@ -38,10 +41,19 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processes
  */
-function runGame() {
+function runGame(gameType) {
+
     //creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        // throw an error and stop the game
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -60,8 +72,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 
